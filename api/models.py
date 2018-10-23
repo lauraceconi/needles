a31ef5f5571b55b3e2522e767bace7895fd648f0
Laura Ceconi <laura.cec@hotmail.com>
2018-10-22 18:59:27 -0700

    Inclusão do banco MySql; correção do model Token

--- api/models.py
+++ api/models.py
@@ -4,9 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User, Group
 from django.conf import settings
-from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
 class Usuario(User):
     user = models.OneToOneField(User)
